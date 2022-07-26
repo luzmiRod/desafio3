32324b5ea199dbc4d441f8b019efcb99bf8d0acf
Luzmila Rodriguez <luzmilayamilerodriguez@gmail.com>
2022-07-26 19:33:00 -0300

    Se cambió el componente card por uno de MUI

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,40 @@
-import styles from '../styles/Home.module.css'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import { AddShoppingCart, Favorite } from '@mui/icons-material'
 
-const Card = ({nombre, descripcion, imagen, funcion}) => {
-    return <div className={styles.card}>
-        <img className={styles.img} src={imagen} />
-        <h1>{nombre}</h1>
-        <p>{descripcion}</p>
-        <button onClick={(e)=>funcion(nombre, imagen)} className={styles.boton}>Iniciar</button>
-    </div>
+export default function MultiActionAreaCard(props) {
+  const [favorite, setFavorite] = React.useState(false)
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={props.imagen}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.descrip}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      <IconButton aria-label="favorite" onClick={() => setFavorite(!favorite)}>
+          <Favorite color={favorite ? 'error' : 'inherit'} />
+        </IconButton>
+        <IconButton color="success" aria-label="add to shopping cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
 }
-export default Card
